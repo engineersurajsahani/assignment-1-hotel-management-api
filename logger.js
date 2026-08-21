39e41ffc0c05324ade592ab5bde55dfdc3c83d5c
Ashish Kumar <watsupd2@gmail.com>
2026-08-21 10:56:49 +0530

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,8 @@
+// Middleware for request logging (Extra Task)
+function requestLogger(req, res, next) {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
+  next();
+}
+
+module.exports = requestLogger;
